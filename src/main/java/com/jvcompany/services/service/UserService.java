06ef5b370f4001b73f7cc8ac04f17c4920916daa
Jorge Victor <jorgekvictor26@gmail.com>
2026-01-19 21:12:16 +0100

    DatabaseException

--- src/main/java/com/jvcompany/services/service/UserService.java
+++ src/main/java/com/jvcompany/services/service/UserService.java
@@ -2,8 +2,11 @@ package com.jvcompany.services.service;
 
 import com.jvcompany.services.entities.User;
 import com.jvcompany.services.repositories.UserRepository;
+import com.jvcompany.services.service.exceptions.DatabaseException;
 import com.jvcompany.services.service.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,23 +18,32 @@ public class UserService {
     @Autowired
     UserRepository repository;
 
-    public List<User> findAll(){
+    public List<User> findAll() {
         return repository.findAll();
     }
-    public User findById(Long id){
+
+    public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.orElseThrow(()-> new ResourceNotFoundException(id));
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
-    public User insert(User obj){
+
+    public User insert(User obj) {
         obj = repository.save(obj);
         return obj;
     }
 
-    public void deleteById(Long id){
-        repository.deleteById(id);
+    public void deleteById(Long id) {
+        try {
+            repository.deleteById(id);
+        }catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        }
+        catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
-    public User update(Long id, User user){
+    public User update(Long id, User user) {
         User entity = repository.getReferenceById(id);
         updateData(entity, user);
         return repository.save(entity);
--- src/main/java/com/jvcompany/services/service/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/jvcompany/services/service/exceptions/ResourceNotFoundException.java
@@ -1,6 +1,7 @@
 package com.jvcompany.services.service.exceptions;
 
 public class ResourceNotFoundException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
     public ResourceNotFoundException(Long id)
     {
         super("Resource Not found. Id " + id);
